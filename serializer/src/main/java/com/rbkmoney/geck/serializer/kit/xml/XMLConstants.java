69e171b73616864a83c7b70e5b1891865b55c5b6
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2017-03-16 12:57:26 +0300

    Added object types, constants moved to other class

---
+++ serializer/src/main/java/com/rbkmoney/geck/serializer/kit/xml/XMLConstants.java
@@ -0,0 +1,24 @@
+package com.rbkmoney.geck.serializer.kit.xml;
+
+/**
+ * Created by inalarsanukaev on 16.03.17.
+ */
+public interface XMLConstants {
+    static final byte NONEMPTY_STRUCT = 2;
+    static final byte NONEMPTY_LIST = 4;
+    static final byte NONEMPTY_SET = 6;
+    static final byte NONEMPTY_MAP = 8;
+    public static final String KEY = "key";
+    public static final String VALUE = "value";
+    public static final String ROOT = "root";
+    public static final String ELEMENT = "element";
+    public static final String ATTRIBUTE_TYPE = "type";
+    public static final String LIST = "list";
+    public static final String SET = "set";
+    public static final String MAP = "map";
+    public static final String BOOL = "bool";
+    public static final String DOUBLE = "double";
+    public static final String LONG = "long";
+    public static final String BYTEARRAY = "bytearray";
+    public static final String NULL = "null";
+}
--- serializer/src/main/java/com/rbkmoney/geck/serializer/kit/xml/XMLHandler.java
+++ serializer/src/main/java/com/rbkmoney/geck/serializer/kit/xml/XMLHandler.java
@@ -2,6 +2,7 @@ package com.rbkmoney.geck.serializer.kit.xml;
 
 import com.rbkmoney.geck.serializer.ByteStack;
 import com.rbkmoney.geck.serializer.StructHandler;
+import static com.rbkmoney.geck.serializer.kit.xml.XMLConstants.*;
 
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -16,17 +17,7 @@ import java.util.Base64;
  */
 public class XMLHandler implements StructHandler<Writer> {
 
-    static final byte NONEMPTY_STRUCT = 2;
-    static final byte NONEMPTY_LIST = 4;
-    static final byte NONEMPTY_SET = 6;
-    static final byte NONEMPTY_MAP = 8;
-    public static final String KEY = "key";
-    public static final String VALUE = "value";
-    public static final String ROOT = "root";
-    public static final String ELEMENT = "element";
-
     private ByteStack stack = new ByteStack();
-
     private Writer writer = new StringWriter();
     private XMLStreamWriter out;
     private boolean isClosed;
@@ -41,21 +32,24 @@ public class XMLHandler implements StructHandler<Writer> {
         }
     }
 
-    private void writeStartElement() {
-        if (!stack.isEmpty()) {
-            byte x = stack.peek();
-            if (x == NONEMPTY_LIST || x == NONEMPTY_SET) {
-                try {
+    private void writeStartElement(String type) {
+        try {
+            if (!stack.isEmpty()) {
+                byte x = stack.peek();
+                if (x == NONEMPTY_LIST || x == NONEMPTY_SET) {
                     out.writeStartElement(ELEMENT);
-                } catch (XMLStreamException e) {
-                    throw new RuntimeException("Unknown error", e);
                 }
             }
+            if (type != null) {
+                out.writeAttribute(ATTRIBUTE_TYPE, type);
+            }
+        } catch (XMLStreamException e) {
+            throw new RuntimeException("Unknown error", e);
         }
     }
-    private void writeValue(String value){
+    private void writeValue(String value, String type){
         try {
-            writeStartElement();
+            writeStartElement(type);
             out.writeCharacters(value.toString());
             out.writeEndElement();
         } catch (XMLStreamException e) {
@@ -72,7 +66,7 @@ public class XMLHandler implements StructHandler<Writer> {
 
     @Override
     public void beginStruct(int size) throws IOException {
-        writeStartElement();
+        writeStartElement(null);
         stack.push(NONEMPTY_STRUCT);
     }
 
@@ -84,7 +78,7 @@ public class XMLHandler implements StructHandler<Writer> {
 
     @Override
     public void beginList(int size) throws IOException {
-        writeStartElement();
+        writeStartElement(LIST);
         stack.push(NONEMPTY_LIST);
     }
 
@@ -96,7 +90,7 @@ public class XMLHandler implements StructHandler<Writer> {
 
     @Override
     public void beginSet(int size) throws IOException {
-        writeStartElement();
+        writeStartElement(SET);
         stack.push(NONEMPTY_SET);
     }
 
@@ -109,6 +103,11 @@ public class XMLHandler implements StructHandler<Writer> {
     @Override
     public void beginMap(int size) throws IOException {
         stack.push(NONEMPTY_MAP);
+        try {
+            out.writeAttribute(ATTRIBUTE_TYPE, MAP);
+        } catch (XMLStreamException e) {
+            throw new RuntimeException("Unknown error", e);
+        }
     }
 
     @Override
@@ -152,32 +151,32 @@ public class XMLHandler implements StructHandler<Writer> {
 
     @Override
     public void value(boolean value) throws IOException {
-        writeValue(String.valueOf(value));
+        writeValue(String.valueOf(value), BOOL);
     }
 
     @Override
     public void value(String value) throws IOException {
-        writeValue(value);
+        writeValue(value, null);
     }
 
     @Override
     public void value(double value) throws IOException {
-        writeValue(String.valueOf(value));
+        writeValue(String.valueOf(value), DOUBLE);
     }
 
     @Override
     public void value(long value) throws IOException {
-        writeValue(String.valueOf(value));
+        writeValue(String.valueOf(value), LONG);
     }
 
     @Override
     public void value(byte[] value) throws IOException {
-        writeValue(Base64.getEncoder().encodeToString(value));
+        writeValue(Base64.getEncoder().encodeToString(value), BYTEARRAY);
     }
 
     @Override
     public void nullValue() throws IOException {
-        writeValue("null");
+        writeValue(NULL, null);
     }
 
     @Override
