004a393684479ca29d2e31e00f857e9a450a6aff
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2017-03-16 13:01:10 +0300

    fix constant's modifiers

--- serializer/src/main/java/com/rbkmoney/geck/serializer/kit/xml/XMLConstants.java
+++ serializer/src/main/java/com/rbkmoney/geck/serializer/kit/xml/XMLConstants.java
@@ -4,21 +4,21 @@ package com.rbkmoney.geck.serializer.kit.xml;
  * Created by inalarsanukaev on 16.03.17.
  */
 public interface XMLConstants {
-    static final byte NONEMPTY_STRUCT = 2;
-    static final byte NONEMPTY_LIST = 4;
-    static final byte NONEMPTY_SET = 6;
-    static final byte NONEMPTY_MAP = 8;
-    public static final String KEY = "key";
-    public static final String VALUE = "value";
-    public static final String ROOT = "root";
-    public static final String ELEMENT = "element";
-    public static final String ATTRIBUTE_TYPE = "type";
-    public static final String LIST = "list";
-    public static final String SET = "set";
-    public static final String MAP = "map";
-    public static final String BOOL = "bool";
-    public static final String DOUBLE = "double";
-    public static final String LONG = "long";
-    public static final String BYTEARRAY = "bytearray";
-    public static final String NULL = "null";
+    byte NONEMPTY_STRUCT = 2;
+    byte NONEMPTY_LIST = 4;
+    byte NONEMPTY_SET = 6;
+    byte NONEMPTY_MAP = 8;
+    String KEY = "key";
+    String VALUE = "value";
+    String ROOT = "root";
+    String ELEMENT = "element";
+    String ATTRIBUTE_TYPE = "type";
+    String LIST = "list";
+    String SET = "set";
+    String MAP = "map";
+    String BOOL = "bool";
+    String DOUBLE = "double";
+    String LONG = "long";
+    String BYTEARRAY = "bytearray";
+    String NULL = "null";
 }
